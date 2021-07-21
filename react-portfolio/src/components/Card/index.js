86a4dce8826e57a20b57461ae19f6a2004d4b8dc
okni-c <oknistay@gmail.com>
2021-07-21 17:43:40 -0500

    Began cards for projects

--- react-portfolio/src/components/Card/index.js
+++ react-portfolio/src/components/Card/index.js
@@ -3,81 +3,77 @@ import React from 'react';
 function Card() {
 
     return (
-        <>
-            <div class="col">
-                <div class="card shadow-sm">
-                    <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg"
-                        role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false">
-                        <title>Placeholder</title>
-                        <rect width="100%" height="100%" fill="#59B640"/><text x="50%" y="50%" fill="#000000"
-                            dy=".3em">sweat-nashville</text>
-                    </svg>
-                    <div class="card-body">
-                        <div class="d-flex justify-content-between align-items-center">
-                            <div class="btn-group">
-                                <form action='https://github.com/okni-c/sweat-nashville'>
-                                    <input type="submit" class="btn btn-sm btn-outline-secondary" value="View on GitHub" />
-                                </form>
-                            </div>
-                            <small class="text-muted">Created June 22, 2021</small>
-                        </div>
-                    </div>
+        <>  
+            {/* Start project card */}
+            <div class="row g-0 position-relative">
+                <div class="col-md-6 mb-md-0 p-md-4">
+                    <img src="..." class="w-100" alt="..." />
                 </div>
-                <div class="card shadow-sm">
-                    <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg"
-                        role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false">
-                        <title>Placeholder</title>
-                        <rect width="100%" height="100%" /><text x="50%" y="50%" fill="#000000"
-                            dy=".3em">Project Name</text>
-                    </svg>
-                    <div class="card-body">
-                        <div class="d-flex justify-content-between align-items-center">
-                            <div class="btn-group">
-                                <form action='{data.html_url}'>
-                                    <input type="submit" class="btn btn-sm btn-outline-secondary" value="View on GitHub" />
-                                </form>
-                            </div>
-                            <small class="text-muted">Created</small>
-                        </div>
-                    </div>
-                </div>
-                <div class="card shadow-sm">
-                    <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg"
-                        role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false">
-                        <title>Placeholder</title>
-                        <rect width="100%" height="100%" /><text x="50%" y="50%" fill="#000000"
-                            dy=".3em">Project Name</text>
-                    </svg>
-                    <div class="card-body">
-                        <div class="d-flex justify-content-between align-items-center">
-                            <div class="btn-group">
-                                <form action='{data.html_url}'>
-                                    <input type="submit" class="btn btn-sm btn-outline-secondary" value="View on GitHub" />
-                                </form>
-                            </div>
-                            <small class="text-muted">Created</small>
-                        </div>
-                    </div>
-                </div>
-                <div class="card shadow-sm">
-                    <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg"
-                        role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false">
-                        <title>Placeholder</title>
-                        <rect width="100%" height="100%" /><text x="50%" y="50%" fill="#000000"
-                            dy=".3em">Project Name</text>
-                    </svg>
-                    <div class="card-body">
-                        <div class="d-flex justify-content-between align-items-center">
-                            <div class="btn-group">
-                                <form action='{data.html_url}'>
-                                    <input type="submit" class="btn btn-sm btn-outline-secondary" value="View on GitHub" />
-                                </form>
-                            </div>
-                            <small class="text-muted">Created</small>
-                        </div>
-                    </div>
+                <div class="col-md-6 p-4 ps-md-0">
+                    <h5 class="mt-0">Project Title</h5>
+                    <p>Description</p>
+                    <a href="#" class="stretched-link">GitHub Link</a>
                 </div>
             </div>
+            <hr />
+            {/* End project card */}
+            
+            {/* Start project card */}
+            <div class="row g-0 position-relative">
+                <div class="col-md-6 mb-md-0 p-md-4">
+                    <img src="..." class="w-100" alt="..." />
+                </div>
+                <div class="col-md-6 p-4 ps-md-0">
+                    <h5 class="mt-0">Project Title</h5>
+                    <p>Description</p>
+                    <a href="#" class="stretched-link">GitHub Link</a>
+                </div>
+            </div>
+            <hr />
+            {/* End project card */}
+            
+            {/* Start project card */}
+            <div class="row g-0 position-relative">
+                <div class="col-md-6 mb-md-0 p-md-4">
+                    <img src="..." class="w-100" alt="..." />
+                </div>
+                <div class="col-md-6 p-4 ps-md-0">
+                    <h5 class="mt-0">Project Title</h5>
+                    <p>Description</p>
+                    <a href="#" class="stretched-link">GitHub Link</a>
+                </div>
+            </div>
+            <hr />
+            {/* End project card */}
+            
+            {/* Start project card */}
+            <div class="row g-0 position-relative">
+                <div class="col-md-6 mb-md-0 p-md-4">
+                    <img src="..." class="w-100" alt="..." />
+                </div>
+                <div class="col-md-6 p-4 ps-md-0">
+                    <h5 class="mt-0">Project Title</h5>
+                    <p>Description</p>
+                    <a href="#" class="stretched-link">GitHub Link</a>
+                </div>
+            </div>
+            <hr />
+            {/* End project card */}
+            
+            {/* Start project card */}
+            <div class="row g-0 position-relative">
+                <div class="col-md-6 mb-md-0 p-md-4">
+                    <img src="..." class="w-100" alt="..." />
+                </div>
+                <div class="col-md-6 p-4 ps-md-0">
+                    <h5 class="mt-0">Project Title</h5>
+                    <p>Description</p>
+                    <a href="#" class="stretched-link">GitHub Link</a>
+                </div>
+            </div>
+            <hr />
+            {/* End project card */}
+            
         </>
     );
 
