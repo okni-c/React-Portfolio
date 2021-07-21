658393c2bd298398ff5d6010614adb32686319dd
okni-c <oknistay@gmail.com>
2021-07-21 16:57:06 -0500

    Added more detail to resume

--- react-portfolio/src/components/Resume/index.js
+++ react-portfolio/src/components/Resume/index.js
@@ -25,16 +25,37 @@ function Resume() {
                 <p class="lead">MyGoTrainer • Mount Juliet, Tn
                     02/2021 - Present
                 </p>
+                <ul class="lead">
+                    <li> - Participated in developing responsive public facing sites using Node, Express.js, and Handlebars on the
+                        MyGoTrainer platform.</li>
+                    <li> - Worked with graphic designer to implement custom content on the front end</li>
+                    <li> - Utilized version control with Git and Github.</li>
+                </ul>
+                <br />
                 <h6 class="lead">Team Lead & Material Operator III</h6>
                 <p class="lead">CEVA Logistics • Mount Juliet, Tn
                     11/2020 - 03/2021
-
                 </p>
+                <ul class="lead">
+                    <li> - Participated in continuous improvement by designing specific web applications for temporary work processes</li>
+                    <li> - Trained new staff on job duties, company policies and safety procedures for rapid onboarding.
+                    </li>
+                    <li> - Assembled, tested, and shipped hundreds of POS system computers for NCR.</li>
+                </ul>
+                <br />
                 <h6 class="lead">Virtual Administrator</h6>
                 <p class="lead">VForYouHR • Brentwood, Tn
                     08/2018 - 09/2020
-
                 </p>
+                <ul class="lead">
+                    <li> - Interpreted management directives to define and document administrative staff processes.
+                    </li>
+                    <li> - Offered virtual enterprise administration services in
+                        HealthStream to a variety of healthcare clients including HCA, Sarah Cannon, and Wellpath.</li>
+                    <li> - Utilized web services, such as Sales Force, to assist everyday customer requests.
+                    </li>
+                </ul>
+                <br />
                 <hr />
                 <h3 >Education</h3>
                 <h6 class="lead">Full Stack Development Bootcamp Certificate</h6>
