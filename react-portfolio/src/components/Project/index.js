036d74df95b9139a102990a5fb250ffa57349b7e
okni-c <oknistay@gmail.com>
2021-07-21 17:44:02 -0500

    Project edited for card injection

--- react-portfolio/src/components/Project/index.js
+++ react-portfolio/src/components/Project/index.js
@@ -14,17 +14,10 @@ function Project() {
                             </div>
                         </div>
                     </div>
-                </div>
-
-                <div class="container">
-                    <div class="row">
-                        <div class="col-12">
-                            <Card />
-                        </div>
-                    </div>
+                    <hr />
+                    <Card />
                 </div>
             </section>
-
         </>
     );
 }
