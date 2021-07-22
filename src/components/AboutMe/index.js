c5151056b655f926ed2134ef51b4d01ce9a8dccb
okni-c <oknistay@gmail.com>
2021-07-21 20:24:33 -0500

    added picture of me

--- src/components/AboutMe/index.js
+++ src/components/AboutMe/index.js
@@ -5,6 +5,8 @@ function AboutMe() {
     return (
         <>
             <div class="container section-padding-80 clearfix">
+                <img src="assets/img/bg-img/me.png" class="img-thumbnail" alt="Portrait" />
+                <br /><br />
                 <h1 class="display-4">About Me</h1>
                 <hr />
                 <h3 class="display-5">Summary</h3>
@@ -15,7 +17,7 @@ function AboutMe() {
                 <h3 class="display-5">Personal Projects</h3>
                 <p class="lead">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                 <hr />
-                
+
             </div>
             <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
         </>
