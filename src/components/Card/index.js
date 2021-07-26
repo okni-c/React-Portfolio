184d0b434661ec9472c8f84ae16308d2b5298af2
okni-c <oknistay@gmail.com>
2021-07-25 23:27:19 -0500

    add final project to portfolio

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -5,6 +5,21 @@ function Card() {
     return (
         <>
             {/* Start project card */}
+            <a href="https://github.com/joshacross/final-project">
+                <div class="row g-0 position-relative">
+                    <div class="col-md-6 mb-md-0 p-md-4">
+                        <img src="/assets/img/project-img/ar.png" class="w-100" alt="..." />
+                    </div>
+                    <div class="col-md-6 p-4 ps-md-0">
+                        <h5 class="mt-0">Augumented Retail</h5>
+                        <p>An ecommerce React application utilizing cutting edge technologies in: augmented reality, 3D rendering, database managment and so much more.</p>
+                    </div>
+                </div>
+            </a>
+            <hr />
+            {/* End project card */}
+
+            {/* Start project card */}
             <a href="https://github.com/okni-c/sweat-nashville">
                 <div class="row g-0 position-relative">
                     <div class="col-md-6 mb-md-0 p-md-4">
